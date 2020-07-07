6bcc59fad27c4d13c185bed8a3f8642a481ccd26
jkolda <jkolda@casaleproject.cz>
2020-07-07 16:41:47 +0200

    Stage 4 finished

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,17 +1,41 @@
 # write your code here
+def operation(x):
+    input_line = list(x.split())
+    operands = []
+    operators = []
+    total = int(input_line[0])
+
+    for element in range(len(input_line)):
+        if input_line[element].lstrip('-').isnumeric():
+            operands.append(int(input_line[element]))
+        else:
+            operators.append(add_or_sub(input_line[element]))
+
+    for element in range(0, len(operators)):
+        if operators[element] == '+':
+            total += operands[element + 1]
+        elif operators[element] == '-':
+            total -= operands[element + 1]
+    return total
+
+
+def add_or_sub(operator):
+    if operator.count('-') % 2 == 0:
+        return '+'
+    if operator.count('-') % 2 == 1:
+        return '-'
+    return print('input an operator')
+
+
 while True:
-    input_line = input('')
-    if input_line == '/exit':
+    raw_input = input('')
+    if raw_input == '/exit':
         print('Bye!')
         break
-    elif input_line == '/help':
+    elif raw_input == '/help':
         print('The program calculates the sum of numbers')
-        pass
-    elif not input_line:
-        pass
+        continue
+    elif not raw_input:
+        continue
     else:
-        x = list(map(int, input_line.split()))
-        total = 0
-        for element in range(len(x)):
-            total = total + x[element]
-        print(total)
+        print(operation(raw_input))
