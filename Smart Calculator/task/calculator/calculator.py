615a94e9919180f349a513d55f3047462d1763b3
jkolda <jkolda@casaleproject.cz>
2020-07-07 12:56:29 +0200

    Stage 3 finished

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,6 +1,17 @@
 # write your code here
-x = list(map(int, input("").split()))
-total = 0
-for element in range(len(x)):
-    total = total + x[element]
-print(total)
+while True:
+    input_line = input('')
+    if input_line == '/exit':
+        print('Bye!')
+        break
+    elif input_line == '/help':
+        print('The program calculates the sum of numbers')
+        pass
+    elif not input_line:
+        pass
+    else:
+        x = list(map(int, input_line.split()))
+        total = 0
+        for element in range(len(x)):
+            total = total + x[element]
+        print(total)
